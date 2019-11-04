4ef309517a4c5856c871cbe303832fb1f58585f2
push <47545876+ravenlandpush@users.noreply.github.com>
2019-11-04 00:25:21 +0000

    Update getlostipfs.sh

--- getlostipfs.sh
+++ getlostipfs.sh
@@ -6,6 +6,7 @@
 # Run this script to try and get them back
 # Date: 03/11/2019
 
+whoami=$(whoami)
 mkdir ~/.lostipfs && cd ~/.lostipfs
 while true; do
 rm ~/.lostipfs/latest.lost.ipfs.txt
@@ -14,7 +15,7 @@ wget https://bootstrap.ravenland.org/latest.lost.ipfs.txt -P ~/.lostipfs
 while IFS= read -r line; do
 echo "Retrieving lost ipfs hash $line"
 ipfs pin add $line --timeout 200s
-done < ~/.lostipfs/latest.lost.ipfs.txt
+done < "/home/$whoami/.lostipfs/latest.lost.ipfs.txt"
 
 sleep 100
 done
